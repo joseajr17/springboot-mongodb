47d541192adacd44c3444429b8c18f3a8937cd58
José <josejr017@gmail.com>
2023-07-23 12:41:02 -0300

    project created

---
+++ src/test/java/com/joseantonio/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseantonio.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
